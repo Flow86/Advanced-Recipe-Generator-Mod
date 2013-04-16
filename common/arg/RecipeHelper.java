a22402f452153df50a8b225732444bef7f45eebb
Flow86 <github@fd.mytrap.de>
2013-04-16 15:08:00 +0200

    missing license

--- common/arg/RecipeHelper.java
+++ common/arg/RecipeHelper.java
@@ -1,3 +1,15 @@
+/** 
+ * Copyright (C) 2013 Flow86
+ * 
+ * AdvancedRecipeGenerator is open-source.
+ *
+ * It is distributed under the terms of my Open Source License. 
+ * It grants rights to read, modify, compile or run the code. 
+ * It does *NOT* grant the right to redistribute this software or its 
+ * modifications in any form, binary or source, except if expressively
+ * granted by the copyright holder.
+ */
+
 package arg;
 
 import java.util.ArrayList;
